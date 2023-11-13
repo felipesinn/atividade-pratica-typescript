f3af6f2286ba22fb1be6362b55cd53528e229412
felipesinn <felipesinn01@gmail.com>
2023-11-13 16:32:33 -0300

    exercicio 1 corrigido

--- src/exer1.ts
+++ src/exer1.ts
@@ -1,18 +1,16 @@
-function calcularMediaAprovacao(nota1, nota2) {
-    const media = (nota1 + nota2) / 2;
-    const aprovado = media >= 6;
-  
-    return {
-      media: media,
-      aprovado: aprovado
-    };
-  }
-  
-  const resultado = calcularMediaAprovacao(7, 5);
-  console.log(resultado);
-  
+function calcularMedia(nota1: number, nota2: number): { media:number; aprovado: boolean} {
+  const media = (nota1 + nota2) /2 ;
+  const aprovado: boolean = media >= 6;
 
+  return {
+    media: media,
+    aprovado: aprovado
+  };
+}
 
+const resut = calcularMedia (7,6);
+
+console.log(resut)
 // 1. Crie uma função que receba 2 números e retorne um objeto
 // contendo a média e também um indicador booleano de
 // aprovado/reprovado. Considere aprovado com média >= 6.
